b7e8457af494edf371ea95ce1cb37a074789a4ef
merima98 <merimaceranic98@gmail.com>
2020-10-13 00:01:43 +0200

    Adding some info about ES modules

--- response-handler.js
+++ response-handler.js
@@ -1,11 +1,16 @@
 import fs from 'fs';
-// const fs = require('fs');
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const resHandler = (req, res, next) => {
-    fs.readFile('my-page.html', 'utf8', (err, data) => {
-        res.send(data);
-    });
+    // fs.readFile('my-page.html', 'utf8', (err, data) => {
+    //     res.send(data);
+    // });
+    res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
 // module.exports = resHandler;
