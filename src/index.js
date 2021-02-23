56fe818c310a37c311db68f8d55c9f5d109bf639
merima98 <merimaceranic98@gmail.com>
2021-02-23 11:16:18 +0100

    Setting up server

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(PORT);
